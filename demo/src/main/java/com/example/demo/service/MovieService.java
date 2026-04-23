551e30b4b470e3e4d3e251e6d077ce3ab460e602
bongjk220 <bongjk220@gmail.com>
2026-04-23 13:26:27 +0900

    [재근] 특수문자 제거

--- demo/src/main/java/com/example/demo/service/MovieService.java
+++ demo/src/main/java/com/example/demo/service/MovieService.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.dto.Movie;
 import com.example.demo.repository.MovieRepository;
+import com.example.demo.util.HtmlEntityUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -36,8 +37,14 @@ public class MovieService {
             List<Movie> list = Arrays.asList(
                     mapper.readValue(file, Movie[].class)
             );
+            
+            // HTML 엔티티 정제
+            List<Movie> cleanedList = list.stream()
+                    .map(this::cleanMovieData)
+                    .toList();
+            
             repo.deleteAll();
-            repo.saveAll(list);
+            repo.saveAll(cleanedList);
         } catch (Exception e) {
             // If JSON parsing fails, create sample data
             createSampleData();
@@ -70,6 +77,24 @@ public class MovieService {
         
         repo.deleteAll();
         repo.saveAll(sampleMovies);
+    }
+
+    /**
+     * 영화 데이터의 HTML 엔티티 정제
+     * @param movie 정제할 영화 데이터
+     * @return 정제된 영화 데이터
+     */
+    private Movie cleanMovieData(Movie movie) {
+        return Movie.builder()
+                .boxoRank(movie.getBoxoRank())
+                .movieNm(HtmlEntityUtils.cleanText(movie.getMovieNm()))
+                .rfilmDe(HtmlEntityUtils.cleanText(movie.getRfilmDe()))
+                .movieStatNm(HtmlEntityUtils.cleanText(movie.getMovieStatNm()))
+                .admisClassNm(HtmlEntityUtils.cleanText(movie.getAdmisClassNm()))
+                .imgPathNm(HtmlEntityUtils.cleanText(movie.getImgPathNm()))
+                .movieSynopCn(HtmlEntityUtils.cleanText(movie.getMovieSynopCn()))
+                .playTime(HtmlEntityUtils.cleanText(movie.getPlayTime()))
+                .build();
     }
 
     public List<Movie> getAll() {
